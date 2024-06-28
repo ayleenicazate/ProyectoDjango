ad3372b63cd88e74f8e3935f800f47c07f2a5cb1
ayleenicazate <ayl.guajardo@duocuc.cl>
2024-06-28 12:14:36 -0400

    added SDK transbank and requirements.txt with command pip freeze

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Ayleen\Desktop\BlogClone\venv
+set VIRTUAL_ENV=C:\Users\Ayleen\Desktop\BlogDjango\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
