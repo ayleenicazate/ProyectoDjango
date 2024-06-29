65493c4904d5da3f7bf3911ec5146b93ab2e445f
ayleenicazate <ayl.guajardo@duocuc.cl>
2024-06-29 12:28:43 -0400

    commit

--- Django-Blog/blogproj/shop/models.py
+++ Django-Blog/blogproj/shop/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Producto(models.Model):
+    id_producto = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length = 64)
     precio = models.IntegerField()
 
